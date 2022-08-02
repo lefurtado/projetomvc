232b2152926c6735bb1925c3f2f64421575a085d
Leandro Furtado <le_furtado@outlook.com.br>
2022-08-02 20:57:01 -0300

    modificados os arquivos de rota

---
+++ src/controllers/pageController.js
@@ -0,0 +1,18 @@
+const home = (req, res) => {
+    res.send('minha rota');
+};
+
+const dogs = (req, res) => {
+    
+};
+const cats = (req, res) => {
+
+};
+const fishes = (req, res) => {
+
+};
+
+module.exports = home;
+module.exports = dogs;
+module.exports = cats;
+module.exports = fishes;
---
+++ src/controllers/searchController.js
@@ -0,0 +1,5 @@
+const search  = (req, res) => {
+    // res.send('teste')
+}
+
+module.exports = search;
